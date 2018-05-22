ff86aafee007a47d4924e4d74df67a8bfdc32d3a
David Fries <david.fries@uni-weimar.de>
2018-05-22 20:40:02 +0200

    draws lines

--- src/infovis/paracoords/View.java
+++ src/infovis/paracoords/View.java
@@ -1,8 +1,12 @@
 package infovis.paracoords;
 
+import infovis.scatterplot.Data;
 import infovis.scatterplot.Model;
 
-import java.awt.Graphics;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JPanel;
 
@@ -11,6 +15,26 @@ public class View extends JPanel {
 
 	@Override
 	public void paint(Graphics g) {
+		Graphics2D g2D = (Graphics2D) g;
+		g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+		model.setDim(model.getLabels().size());
+		int dim = model.getDim();
+		g2D.setColor(Color.black);
+		for(int i = 0; i < dim; i++) {
+			g2D.drawLine((getWidth()/dim)*(i+1),0,(getWidth()/dim)*(i+1),getHeight());
+		}
+		ArrayList list = new ArrayList();
+		for (Data d : model.getList()) {
+			Map<Double, Integer> hm = new HashMap<Double, Integer>();
+			for(int i = 0; i < dim; i++) {
+				//System.out.println(d.getValue(i));
+				hm.put(d.getValue(i),getHeight()-(getHeight()/10)*hm.size());
+
+			}
+			list.add(hm);
+		}
+
+		//System.out.println(model.getList());
 	}
 
 	@Override
